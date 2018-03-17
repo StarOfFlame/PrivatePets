a1dcc41a1109dcfebf2c481e43a78425ef5d02d9
DoooReyn <jl88744653@gmail.com>
2018-03-17 15:26:23 +0800

    rename

--- pets/src/main.lua
+++ pets/src/main.lua
@@ -5,7 +5,7 @@ require "config"
 require "cocos.init"
 
 local function main()
-    require("app.MyApp"):create():run()
+    cc.exports.game = require("app.Game").new()
 end
 
 local status, msg = xpcall(main, __G__TRACKBACK__)
