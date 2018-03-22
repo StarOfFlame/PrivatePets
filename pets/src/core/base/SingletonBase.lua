51096d6a930916b43df8f73e113c039d4b792d4f
DoooReyn <jl88744653@gmail.com>
2018-03-23 01:00:31 +0800

    singleton

--- pets/src/core/base/SingletonBase.lua
+++ pets/src/core/base/SingletonBase.lua
@@ -1,16 +1,8 @@
 local SingletonBase = class('SingletonBase')
 
-function SingletonBase:ctor()
-    self.__instance__ = nil
-    
-end
-
 function SingletonBase:getInstance()
     if not self.__instance__ then
-        self.__inittime__ = checkint(self.__inittime__)
-        self.__inittime__ = self.__inittime__ + 1
         self.__instance__ = self.new()
-        self:tag('__inittime__', self.__inittime__)
     end
     return self.__instance__
 end
