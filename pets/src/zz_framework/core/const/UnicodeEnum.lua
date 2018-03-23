a8cb1711a113e8e56542809b2d601cd5c403a466
DoooReyn <jl88744653@gmail.com>
2018-03-24 02:38:25 +0800

    Unicode Definition

---
+++ pets/src/zz_framework/core/const/UnicodeEnum.lua
@@ -0,0 +1,42 @@
+------------------------------------------------------------------------------------------
+---- Name   : UnicodeEnum
+---- Desc   : Unicode 字符定义
+---- Date   : 2018/03/24
+---- Author : Reyn 
+---- Email  : jl88744653@gmail.com
+------------------------------------------------------------------------------------------
+
+return {
+    FALSE        = '❎',
+    TRUE         = '✅',
+    ERROR        = '❌',
+    CORRECT      = '✔️',
+    WARNING      = '⚠️',
+    FORBIDDEN    = '🚫',
+    UP           = '⬆️',
+    DOWN         = '⬇️',
+    LEFT         = '⬅️',
+    RIGHT        = '➡️',
+    CRY          = '😢',
+    SMILE        = '🙂',
+    HAPPY        = '😊',
+    ANGRY        = '😡',
+    SPEECHLESS   = '😑',
+    TIRED        = '😪',
+    DISAPPOINTED = '😞',
+    SHOCK        = '😲',
+    DISDAIN      = '🙄',
+    BELITTLE     = '👎',
+    PRAISE       = '👍',
+    COOL         = '😎',
+    SHY          = '😊',
+    SLEEP        = '😴',
+    APPLAUSE     = '👏',
+    CHEERUP      = '💪',
+    VERTIGO      = '😓',
+    PITIFUL      = '🤕',
+    FLOWER       = '🌹',
+    HEART_BEATEN = '❤️',
+    HERAT_BROKEN = '💔',
+    GOOD_BYE     = '👋',
+}
