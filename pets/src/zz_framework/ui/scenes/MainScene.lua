654cc2fd57200be27af9031b9d4d83f590e92e94
DoooReyn <jl88744653@gmail.com>
2018-03-26 00:06:52 +0800

    comment

--- pets/src/zz_framework/ui/scenes/MainScene.lua
+++ pets/src/zz_framework/ui/scenes/MainScene.lua
@@ -1,3 +1,11 @@
+------------------------------------------------------------------------------------------
+---- Name   : MainScene
+---- Desc   : 主场景
+---- Date   : 2018/03/25
+---- Author : Reyn 
+---- Email  : jl88744653@gmail.com
+------------------------------------------------------------------------------------------
+
 local MainScene = class('MainScene', SceneBase)
 
 function MainScene:initialize()
@@ -8,6 +16,7 @@ function MainScene:onEnter()
     zz:loadInstance('TestCase'):run()
 end
 
+--[[挂载节点]]
 function MainScene:mountNodes()
     for name, zorder in pairs(UIBase.ZOrder) do
         local node = cc.Node:create()
@@ -16,18 +25,36 @@ function MainScene:mountNodes()
     end
 end
 
+--[[获取挂载节点]]
 function MainScene:getNode(name)
     return self:getChildByName(name)
 end
 
-function MainScene:show(name)
+--[[显示挂载节点]]
+function MainScene:showNode(name)
     local node = self:getNode(name)
     if node then node:setVisible(true) end
 end
 
-function MainScene:hide()
+--[[隐藏挂载节点]]
+function MainScene:hideNode()
     local node = self:getNode(name)
     if node then node:setVisible(false) end
+end
+
+--[[清理挂载节点]]
+function MainScene:cleanNode(name)
+    local node = getNode(name)
+    if node then
+        node:removeAllChildren()
+    end
+end
+
+--[[清理所有挂载节点]]
+function MainScene:cleanAllNodes()
+    for name in pairs(UIBase.ZOrder) do
+        self:getNode(name)
+    end
 end
 
 return MainScene
