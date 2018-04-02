b56c72b71500ca8a5a043d725c9088f26954e8d1
Reyn <jl88744653@gmail.com>
2018-04-02 22:32:44 +0800

    statemgr

---
+++ pets/src/zz_framework/core/manager/StateMgr.lua
@@ -0,0 +1,27 @@
+------------------------------------------------------------------------------------------
+---- Name   : StateMgr
+---- Desc   : 状态管理器
+---- Date   : 2018/04/02
+---- Author : Reyn 
+---- Email  : jl88744653@gmail.com
+------------------------------------------------------------------------------------------
+
+local StateMgr = class('StateMgr')
+
+function StateMgr:ctor()
+
+end
+
+function StateMgr:getState()
+
+end
+
+function StateMgr:setState()
+
+end
+
+function StateMgr:update()
+
+end
+
+return StateMgr
