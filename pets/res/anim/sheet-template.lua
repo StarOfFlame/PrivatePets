18b2119d7c091867805d52839189762aa10a02b5
Reyn <jl88744653@gmail.com>
2018-04-02 21:51:25 +0800

    sheet-template

---
+++ pets/res/anim/sheet-template.lua
@@ -0,0 +1,28 @@
+-----------------------------------------------
+-- 精灵表配置文件 {{texture.fullName}}
+-- created with TexturePacker for cocos-lua
+-----------------------------------------------
+
+local SheetInfo = {}
+setmetatable(SheetInfo, {__index = SpriteSheetBase})
+
+SheetInfo.contentSize      = cc.size({{texture.size.width}}, {{texture.size.height}})
+SheetInfo.source           = "anim/{{texture.fullName}}"
+SheetInfo.premultiplyAlpha = {{settings.premultiplyAlpha}}
+SheetInfo.allowRotation    = {{settings.allowRotation}}
+SheetInfo.ditherType       = {{settings.ditherType}}
+
+SheetInfo.frames = {{% for sprite in allSprites %}
+    {   -- {{sprite.trimmedName}}
+        rect   = cc.rect({{sprite.frameRect.x}}, {{sprite.frameRect.y}}, {{sprite.frameRect.width}}, {{sprite.frameRect.height}}),
+        size   = cc.size({{sprite.untrimmedSize.width}}, {{sprite.untrimmedSize.height}}),
+        rotate = {{sprite.rotated}},
+        offset = cc.p({{sprite.cornerOffset.x}}, {{sprite.cornerOffset.y}})
+    },{% endfor %}
+}
+
+SheetInfo.frameIndex = {{% for sprite in allSprites %} 
+    ["{{settings.textureSubPath}}_{{sprite.trimmedName}}"] = {{ forloop.counter }},{% endfor %}
+}
+
+return SheetInfo
